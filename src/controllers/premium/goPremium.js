858d80602e8b07bfe159b7b6cb1e9932f6fe6274
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-28 20:42:13 +0530

    go premium

--- src/controllers/premium/goPremium.js
+++ src/controllers/premium/goPremium.js
@@ -1,9 +1,15 @@
 const services = require("../../db/dbServiecs");
 
 const goPremium=async(req,res)=>{
+    try{
     if(req.isPremium) return res.status(400).send({message:"already a premium user"})
     await services.updateData(req.decodedId,{is_premium_user:true})
     return res.status(200).send({status:true,message:"you are now a premium user"})
+    }
+    catch(err)
+    {
+        return res.status(500).send({status:false,message:err})
+    }
     
 }
 module.exports={goPremium}
